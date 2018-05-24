a28159e823e910aeff1b1b82b7b9d26cca5ace3c
Andres Garcia Mangas <agmangas@gmail.com>
2018-05-24 20:10:15 +0200

    Implement some static utility methods

--- SerialRFID.cpp
+++ SerialRFID.cpp
@@ -1,3 +1,80 @@
 /*
   SerialRFID.h - Interact with ID-innovations RFID readers using serial
 */
+
+#include "SerialRFID.h"
+
+bool SerialRFID::isPrintableTagChar(char theChar)
+{
+  if (theChar != TAG_CHAR_STX &&
+      theChar != TAG_CHAR_CR &&
+      theChar != TAG_CHAR_LF &&
+      theChar != TAG_CHAR_ETX)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool SerialRFID::isEqualTag(char nTag[], char oTag[])
+{
+  if (strlen(nTag) != LEN_ID_PRINTABLE ||
+      strlen(oTag) != LEN_ID_PRINTABLE)
+  {
+    return false;
+  }
+
+  for (int i = 0; i < LEN_ID; i++)
+  {
+    if (nTag[i] != oTag[i])
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void SerialRFID::findTagInBuffer(char buf[], int bufSize, char newTag[])
+{
+  memset(newTag, 0, LEN_ID);
+
+  if (bufSize < LEN_TAG)
+  {
+    return;
+  }
+
+  int stxIdx = bufSize - LEN_TAG;
+
+  if (buf[stxIdx] != TAG_CHAR_STX)
+  {
+    return;
+  }
+
+  int counter = 0;
+
+  for (int i = stxIdx; i < bufSize; i++)
+  {
+    if (isPrintableTagChar(buf[i]))
+    {
+      // Check if we are reading more bytes than expected
+      if (counter >= (LEN_ID - 1))
+      {
+        memset(newTag, 0, LEN_ID);
+        return;
+      }
+
+      newTag[counter] = buf[i];
+      counter++;
+    }
+  }
+
+  // Check if the output array length is not the expected
+  if (strlen(newTag) != LEN_ID_PRINTABLE)
+  {
+    memset(newTag, 0, LEN_ID);
+  }
+}
--- SerialRFID.h
+++ SerialRFID.h
@@ -11,4 +11,26 @@
 #include "WProgram.h"
 #endif
 
+const char TAG_CHAR_STX = 2;
+const char TAG_CHAR_CR = 13;
+const char TAG_CHAR_LF = 10;
+const char TAG_CHAR_ETX = 3;
+
+// Each tag is 16 bytes but the last four are to be discarded.
+const int LEN_TAG = 16;
+const int LEN_ID = 13;
+const int LEN_ID_PRINTABLE = 12;
+
+class SerialRFID
+{
+public:
+  SerialRFID(Stream &s) : serial(s){};
+  static bool isEqualTag(char nTag[], char oTag[]);
+
+private:
+  Stream &serial;
+  static bool isPrintableTagChar(char theChar);
+  static void findTagInBuffer(char buf[], int bufSize, char newTag[]);
+};
+
 #endif
